5d15269751073cae4c8c4a62e6fb36988714b032
Dmytro <Deynao1996@gmail.com>
2024-06-15 14:09:44 +0300

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className="flex justify-center items-center h-screen w-full">
+      <div className="p-10 border border-white bg-card">
+        <h1 className="text-primary text-center">HomePage</h1>
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
